78519636d7fe6442ead4076e1a4e278656132bbd
allanjames <james.allan@brafton.com>
2018-07-11 15:18:49 -0400

    clearing files to git

---
+++ source/constants.php
@@ -0,0 +1,18 @@
+<?php
+
+$crude = file_get_contents("shop-data.JSON");
+$refined = json_decode($crude);
+
+define('SHOPIFY_PRIVATE_KEY', $refined->shop_private);
+define('SHOPIFY_PRIVATE_PW', $refined->shop_pw);
+define('SHOPIFY_STORE_NAME',$refined->store_name);
+define('SHOPIFY_BLOG_ID', $refined->blog_id);
+define('domain','https://api.brafton.com');
+define('brafton_api',$refined->brafton_api);
+define('video_import',$refined->video);
+if(video_import == true) :
+    define('brafton_private_key',$refined->brafton_private);
+    define('brafton_public_key',$refined->brafton_public);
+endif;
+
+?>
